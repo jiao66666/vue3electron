fba3f3bcdf0022750264758173bcb6bc3a02b231
jiaonewcook <jiaocook@126.com>
2023-08-28 18:11:46 +0800

    element plus 按需引入 OK

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+module.exports = {
+  outputDir: './build',
+  // 和webpapck属性完全一致，最后会进行合并
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components'
+      }
+    },
+    //配置webpack自动按需引入element-plus，
+      plugins: [
+        AutoImport({
+          resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+          resolvers: [ElementPlusResolver()]
+        })
+      ]
+  }
+}
