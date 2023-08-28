a1523d241d5a372efdc9d94f99598a862a5d71bc
jiaonewcook <jiaocook@126.com>
2023-08-28 16:54:06 +0800

    addVuexPersistenState

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,16 @@ import {createStore} from 'vuex'
 import user from './modules/user'
 import app from './modules/app'
 import task from './modules/task'
+import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
     modules: {
       user: user,
       app: app,
       task: task
-    }
+    },
+    plugins: [createPersistedState()]
+
 })
 
 export default store
