8ad88542a03b57a3807df35181375b8bcc346b99
jiaonewcook <jiaocook@126.com>
2023-09-04 09:32:55 +0800

    改进菜谱菜单 UI

--- src/views/dishMenu/index.vue
+++ src/views/dishMenu/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-tabs tab-position="left"  class="demo-tabs">
+    <el-tabs tab-position="left"  class="demo-tabs" style="height:330px;">
         <el-tab-pane v-for="item in menuList" :key='item.id' :label="item.title"><img :src="item.img" class="img"/></el-tab-pane>
     </el-tabs>
 </template>
@@ -8,11 +8,11 @@ import { ref } from 'vue'
 import Image from '@/components/Image.vue'
 const menuList=ref([
     {id:1,title:'红烧鸡翅',img:'http://121.37.130.1/sample.jpg'},
-    {id:2,title:'宫爆鸡丁',img:'http://121.37.130.1/sample.jpg'},
-    {id:3,title:'麻辣火鸡',img:'http://121.37.130.1/sample.jpg'},  
-    {id:4,title:'西湖醋鱼',img:'http://121.37.130.1/sample.jpg'},   
-    {id:5,title:'北京烤鸭',img:'http://121.37.130.1/sample.jpg'}, 
-    {id:6,title:'莲藕香醋',img:'http://121.37.130.1/sample.jpg'}, 
+    {id:2,title:'宫爆鸡丁',img:'http://121.37.130.1/sample2.jpg'},
+    {id:3,title:'麻辣火鸡',img:'http://121.37.130.1/sample3.jpg'},  
+    {id:4,title:'西湖醋鱼',img:'http://121.37.130.1/sample4.jpg'},   
+    {id:5,title:'北京烤鸭',img:'http://121.37.130.1/sample5.jpg'}, 
+    {id:6,title:'莲藕香醋',img:'http://121.37.130.1/sample6.jpg'}, 
     {id:7,title:'麻婆豆腐',img:'http://121.37.130.1/sample.jpg'}, 
     {id:8,title:'鲍翅鱼类',img:'http://121.37.130.1/sample.jpg'}, 
     {id:9,title:'火烧机场',img:'http://121.37.130.1/sample.jpg'}, 
